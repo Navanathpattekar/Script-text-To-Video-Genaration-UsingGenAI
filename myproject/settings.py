356991e1ccac1bfa68c5fa641b1006a0f72fb55e
Navanath Pattekar <navanath@example.com>
2026-05-24 20:40:02 +0530

    Fixed templates path

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,9 +58,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'C:/Users/HP/OneDrive/Documents/major project/env/myproject/myapp/Templates/',
-            ],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
