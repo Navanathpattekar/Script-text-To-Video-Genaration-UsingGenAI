876b478e807e28016b3c6bfc90892f487db9173e
Navanath Pattekar <navanath@example.com>
2026-05-25 15:29:34 +0530

    fix media folder render issue

--- myproject/settings.py
+++ myproject/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media").replace("\\\\", "/").replace("\\", "/")
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
